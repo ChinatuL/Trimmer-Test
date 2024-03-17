04d5c4aa6b3bfc32870e04f436fa67acc98bc550
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2024-03-17 14:54:12 +0100

    style: modify error and 404 pages, add responsiveness

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -16,13 +16,20 @@ export default function Error({
     }, [error]);
 
     return (
-        <div className='flex justify-center items-center bg-auth-bg bg-cover w-full h-[100vh] text-zinc-50'>
-            <div className='flex flex-col justify-center items-center py-8 px-8 rounded bg-darkBlue text-zinc-50 w-[80vw] h-[90vh] mx-auto'>
+        <div className='flex justify-center items-center md:bg-auth-bg bg-cover w-full h-[100vh] text-zinc-50'>
+            <div className='flex flex-col justify-center items-center py-8 px-8 rounded bg-darkBlue text-zinc-50 w-[100vw] h-[100vh] md:w-[80vw] md:h-[90vh] mx-auto'>
                 <Image src={errorBg} alt='' />
-                <div className="pt-8 text-center">
-                    <h1 className="font-semibold text-3xl">OOPS!</h1>
-                    <p className="text-lg pb-4">An error occurred but it&apos;s not your fault</p>
-                    <button onClick={() => reset()} className="px-4 py-2 bg-purple rounded-lg hover:opacity-70 transitionEase">Try again</button>
+                <div className='pt-8 text-center'>
+                    <h1 className='font-semibold text-3xl'>OOPS!</h1>
+                    <p className='text-lg pb-4'>
+                        An error occurred but it&apos;s not your fault
+                    </p>
+                    <button
+                        onClick={() => reset()}
+                        className='px-4 py-2 bg-purple rounded-lg hover:opacity-70 transitionEase'
+                    >
+                        Try again
+                    </button>
                 </div>
             </div>
         </div>
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,17 @@
 import Link from "next/link";
+
 export default function NotFound() {
     return (
-        <div className='bg-auth-bg bg-cover w-full h-[100vh] text-zinc-50'>
-            <h2>Not Found</h2>
-            <p>Could not find requested resource</p>
-            <Link href='/'>Return Home</Link>
+        <div className='flex justify-center items-center md:bg-auth-bg bg-cover w-full h-[100vh] text-zinc-50'>
+            <div className='flex flex-col justify-center md:justify-end items-center py-8 px-8 rounded bg-darkBlue text-zinc-50 w-[100vw] h-[100vh] md:w-[80vw] md:h-[90vh] mx-auto bg-[url("/icons/404-mobile.svg")] md:bg-[url("/icons/404.svg")] bg-no-repeat bg-top md:bg-center'>
+                <h1 className='font-semibold text-3xl'>OOPS</h1>
+                <p className='text-lg pb-4'>Looks like you&apos;re lost</p>
+                <Link href='/'>
+                    <div className='px-4 py-2 bg-purple rounded-lg hover:opacity-70 transitionEase'>
+                        Take Me Home
+                    </div>
+                </Link>
+            </div>
         </div>
     );
 }
