9dec2275f4fc37b1fbd6df49456bbb831d6c61c2
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2024-03-27 13:57:16 +0100

    feat: add profile page

--- src/app/ui/back-to-home-btn.tsx
+++ src/app/ui/back-to-home-btn.tsx
@@ -6,7 +6,7 @@ export default function BackToHomeButton() {
         <div className='absolute top-2 left-2 '>
             <Link
                 href='/'
-                className='flex gap-2 items-center px-4 py-2 rounded-md bg-purple text-zinc-50 z-10 transitionEase hover:opacity-75'
+                className='flex gap-2 items-center px-4 py-2 rounded-md bg-purple text-zinc-50 z-30 transitionEase hover:opacity-75'
             >
                 <Image src={leftArrowIcon} alt='' />
                 <span className='hidden lg:block'>Back Home</span>
